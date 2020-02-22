1a9aca631d36c6458f5a5ab78e28837ce4697427
charlesvg <charles.vangoidsenhoven@gmail.com>
2020-02-22 10:11:21 +0100

    Working puppeteer

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('Hello world');
+import * as puppeteer from "puppeteer-core";
+
+(async () => {
+    const browser = await puppeteer.launch({
+        executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+    });
+    const page = await browser.newPage();
+    await page.goto('https://google.com');
+    await page.screenshot({path: 'example.png'});
+
+    await browser.close();
+})();
